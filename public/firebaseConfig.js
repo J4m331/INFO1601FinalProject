c9e6c033445bdf2ab81099535f45d1c06e0b9fc4
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-11 15:48:20 -0400

    project remake

---
+++ public/firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-5TI2nM6J0nTiV5vIGvxSPHkPfA51Q5o",
+    authDomain: "uwicentralhub-f5cf4.firebaseapp.com",
+    projectId: "uwicentralhub-f5cf4",
+    storageBucket: "uwicentralhub-f5cf4.appspot.com",
+    messagingSenderId: "351960244221",
+    appId: "1:351960244221:web:60197735aadec87ffdc394",
+    measurementId: "G-K7KHHT09R4"
+  };
+  
+  
+
+export default firebaseConfig
