251155ce19a4b4ba26cf5ad8fd748b8c6392d861
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-13 14:48:31 -0400

    routing start

--- public/main.js
+++ public/main.js
@@ -11,3 +11,22 @@ generateQRC("YES");
 
 //The following is the HTML to use the above function
 //<div id="QRC"></div>
+
+async function route(title, url){
+    document.title = title;
+    let display = document.querySelector('#display');
+    if(url === null){
+        display.innerHTML = '';
+    }
+    else{
+        let responce = await fetch(url);
+        display.innerHTML = await responce.text();
+    }
+}
+
+function getClick(event){
+    let site = event.target;
+    let text = site.text;
+    let url = site.href;
+    route(text,url);
+}
