dc1d62c57deb4f3e09d3e48cc9da6541b5d13fe5
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-12 20:33:42 -0400

    QR Code generator

--- public/main.js
+++ public/main.js
@@ -1,25 +1,12 @@
-import { signIn, logout, auth, setAuthListeners } from './auth.js';
 
-let loginBtn = document.querySelector('#loginBtn');
-let logoutBtn = document.querySelector('#logoutBtn');
-let reviewForm = document.querySelector('#reviewForm');
-
-function setLoggedInUI(){
-    M.toast({html: 'Logged In!'})
-    document.querySelector('#user').innerHTML = `Signed in Anonymously`;
-    document.querySelector('#loginBtn').style.display = 'none';
-    document.querySelector('#logoutBtn').style.display = 'inline-block';
+function generateQRC(link){
+    let result = document.querySelector('#QRC');
+    let img = '';
+    img += `<img src="https://api.qrserver.com/v1/create-qr-code/?data=${link}&amp;size=100x100" alt="" title="" />`;
+    //API Link format => API->Data(Link)->size(Pixels)
+    result.innerHTML = img;
 }
+generateQRC("YES");
 
-function setLoggedOutUI(){
-    M.toast({html: 'Logged Out!'})
-    document.querySelector('#user').innerHTML = '';
-    document.querySelector('#loginBtn').style.display = 'inline-block';
-    document.querySelector('#logoutBtn').style.display = 'none';
-}
-
-//attatch listeners
-logoutBtn.addEventListener('click', logout);
-loginBtn.addEventListener('click', signIn);
-reviewForm.addEventListener('submit', submitReivew);
-setAuthListeners(setLoggedInUI, setLoggedOutUI);
+//The following is the HTML to use the above function
+//<div id="QRC"></div>
