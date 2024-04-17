6c716add2c81b916123b534e63b9297a99784bbe
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-16 20:10:21 -0400

    Auth changes

--- public/SignUp.js
+++ public/SignUp.js
@@ -1,5 +1,15 @@
-import { getAuth,connectAuthEmulator,signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import { signOut } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth,connectAuthEmulator,signInWithEmailAndPassword, } from 'firebase/auth';
+
+const firebaseConfig = initializeApp({
+    apiKey: "AIzaSyA-5TI2nM6J0nTiV5vIGvxSPHkPfA51Q5o",
+    authDomain: "uwicentralhub-f5cf4.firebaseapp.com",
+    projectId: "uwicentralhub-f5cf4",
+    storageBucket: "uwicentralhub-f5cf4.appspot.com",
+    messagingSenderId: "351960244221",
+    appId: "1:351960244221:web:60197735aadec87ffdc394",
+    measurementId: "G-K7KHHT09R4"
+});
 
 let email_text = document.querySelector('#email_phone');
 let pass_text = document.querySelector('#password');
@@ -9,21 +19,11 @@ let next_button = document.querySelector('#Next');
 const auth = getAuth(firebaseConfig);
 connectAuthEmulator(auth, "http://localhost:5000/");
 
-const createAccount = async () =>{
-
-}
-
 const loginEmail = async () => {
     const Email = email_text.value;
     const Pass = pass_text.value;
-
-    try{
-        const Credential = await signInWithEmailAndPassword(auth,Email,Pass);
-        console.log(Credential.user);
-    }
-    catch(error){
-        console.log(error);
-    }
+    const Credential = await signInWithEmailAndPassword(auth,Email,Pass);
+    console.log(Credential.user);
 }
 
 next_button.addEventListener('click',loginEmail);
