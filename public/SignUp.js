0bfebccb4b740145a9d00f206dc89824896601ed
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-17 16:11:59 -0400

    auth change

--- public/SignUp.js
+++ public/SignUp.js
@@ -1,29 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth,connectAuthEmulator,signInWithEmailAndPassword, } from 'firebase/auth';
-
-const firebaseConfig = initializeApp({
-    apiKey: "AIzaSyA-5TI2nM6J0nTiV5vIGvxSPHkPfA51Q5o",
-    authDomain: "uwicentralhub-f5cf4.firebaseapp.com",
-    projectId: "uwicentralhub-f5cf4",
-    storageBucket: "uwicentralhub-f5cf4.appspot.com",
-    messagingSenderId: "351960244221",
-    appId: "1:351960244221:web:60197735aadec87ffdc394",
-    measurementId: "G-K7KHHT09R4"
-});
+const { default: firebase } = require("firebase/compat/app");
 
 let email_text = document.querySelector('#email_phone');
 let pass_text = document.querySelector('#password');
 
 let next_button = document.querySelector('#Next');
 
-const auth = getAuth(firebaseConfig);
-connectAuthEmulator(auth, "http://localhost:5000/");
+const auth = firebase.auth();
 
-const loginEmail = async () => {
-    const Email = email_text.value;
-    const Pass = pass_text.value;
-    const Credential = await signInWithEmailAndPassword(auth,Email,Pass);
-    console.log(Credential.user);
-}
+const provider = new firebase.auth.GoogleAuthProvider();
 
-next_button.addEventListener('click',loginEmail);
+next_button.onclick = () => auth.signInWithPopup(provider);
