24312699c9f6c566d6dbba9530a18864f91aeea8
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-17 23:52:53 -0400

    new auth

---
+++ public/auth.js
@@ -0,0 +1,26 @@
+import {firebaseConfig} from "./firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut , createUserWithEmailAndPassword} from "firebase/auth";
+
+const email_text = document.querySelector('#email_phone');
+const pass_text = document.querySelector('#password');
+
+const email = email_text.value;
+const pass = pass_text.value;
+
+const auth = getAuth();
+
+function createUser(email, pass){
+    createUserWithEmailAndPassword(auth,email,pass)
+        .then((userCredential) =>{
+            const user = userCredential.user;
+            console.log("User made success");
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("User not made");
+        });
+}
+
+export {auth, createUser};
