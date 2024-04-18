8ad6e33e4a2294a448e484d48020209c82d9f7d1
Jameel Ali <J1m2e3A4i@gmail.com>
2024-04-18 05:34:25 -0400

    unsuccessful auth changes

--- public/auth.js
+++ public/auth.js
@@ -1,5 +1,6 @@
-import {firebaseConfig} from "./firebaseConfig";
-import { getAuth,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig";
+import { getAuth,signInWithPopup,GoogleAuthProvider,signInAnonymously } from "firebase/auth";
 
 const email_text = document.querySelector('#email_phone');
 const pass_text = document.querySelector('#password');
@@ -7,17 +8,38 @@ const pass_text = document.querySelector('#password');
 const Login = document.querySelector('#Google');
 
 const auth = getAuth();
-const provider = GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-function handleGoogleLogin() {
+async function handleGoogleLogin() {
     signInWithPopup(auth, provider)
     .then((result) => {
-        // Handle successful Google sign-in logic here
-        // (e.g., access user info, redirect to another page)
+
+      // Handle successful Google sign-in logic here
+
+      // (e.g., access user info, redirect to another page)
+
+      const user = result.user;
+
+      console.log(user);
+
+      // Redirect to another page
+
+      window.location.href = "home.html";
     })
     .catch((error) => {
-        // Handle errors during Google sign-in
+    
+      // Handle errors during Google sign-in
+
+      const errorCode = error.code;
+
+      const errorMessage = error.message;
+
+      console.error(errorCode, errorMessage);
     });
+}
+
+function AnonymousLogin(){
+
 }
 
 Login.addEventListener('click', handleGoogleLogin);
